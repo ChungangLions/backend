58955894454a6c3c6a26a9d1ac46b39ea43d3c16
brian <kimchanjoong54@gmail.com>
2025-08-16 17:25:35 +0900

    modify: config, proposals url 수정

--- project/proposals/urls.py
+++ project/proposals/urls.py
@@ -1,6 +1,9 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProposalViewSet
 
 router = DefaultRouter()
 router.register(r"proposals", ProposalViewSet, basename="proposal")
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
