5596b9b5915063d8a43ad94967d7d14075ed1370
brian <kimchanjoong54@gmail.com>
2025-08-16 17:12:29 +0900

    feat:  proposals urls 매핑

---
+++ project/proposals/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProposalViewSet
+
+router = DefaultRouter()
+router.register(r"proposals", ProposalViewSet, basename="proposal")
+urlpatterns = router.urls
